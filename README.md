d5800fa8781d07d92ebe0fa9b3af49b4990bfffd
Jabba Laci <jabba.laci@gmail.com>
2025-02-15 20:55:40 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# NationalFlags
-National flags created with shader
+# National Flags
+
+National flags created with shaders.
+
+I saw a [video](https://www.youtube.com/watch?v=IGTuv_KKLFs) from [Tsoding](https://www.youtube.com/@TsodingDaily), and it gave me the idea to create my country's flag with a shader.
+
+## Hungary's Flag
+
+Here is an example:
+
+![example](hungary/output.png)
+
+The code that generates it is here:
+[hungary/main.nim](hungary/main.nim).
+
+## Contribution
+
+If you want, you can send your national flag
+in the form of a PR. Make a copy of the `hungary/` folder, rename it and customize
+the code.
+
+The program that generates `output.ppm`
+can be written in any language but I'd
+prefer to get Nim solutions. But it's not
+a strict requirement.
+
+The command `make all` must produce
+the resulting PNG, thus don't forget
+to edit the `Makefile`.
---
+++ hungary/main.nim
@@ -0,0 +1,48 @@
+import strformat
+import math
+
+# flag of Hungary:
+# horizontally striped red-white-green national flag
+# Its width-to-length ratio is 2 to 3.
+const
+  unit = 250
+  WIDTH = unit * 3
+  HEIGHT = unit * 2
+
+type
+  RGB = tuple[r, g, b: float]
+
+# https://www.flagcolorcodes.com/hungary
+func hungary(u, v: float): RGB =
+  if v < 1/3:
+    (206 / 255, 41 / 255, 57 / 255)
+  elif v > 2/3:
+    (71 / 255, 112 / 255, 80 / 255)
+  else:
+    (1.0, 1.0, 1.0)
+
+proc main() =
+  let fname = "output.ppm"
+  let f = open(fname, fmWrite)
+  defer: f.close()
+
+  f.writeLine("P6")
+  f.writeLine(&"{WIDTH} {HEIGHT} 255")
+
+  for y in 0 ..< HEIGHT:      # row
+    for x in 0 ..< WIDTH:     # col
+      let
+        u = x / WIDTH
+        v = y / HEIGHT
+        (r, g, b) = hungary(u, v)
+
+      f.write(chr(int(r * 255)))
+      f.write(chr(int(g * 255)))
+      f.write(chr(int(b * 255)))
+
+  echo "END"
+
+# ############################################################################
+
+when isMainModule:
+  main()
